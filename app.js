c63e4ed4bda0f2d22c0ce887c705f006ebe2f492
Matthew Long <longm7676@gmail.com>
2022-11-28 15:50:48 -0600

    created git repo

---
+++ app.js
@@ -0,0 +1,41 @@
+8
+1846
+
+yearNeptuneDiscovered:1846
+yearMarsDoscovered:1659;
+
+// your name is Alejandro and you like purple
+// your name is Melissa and you like green
+// your name is undefined and you like green
+
+// Maya
+// Marisa 
+// Chi
+
+// Raindrops on roses
+// whiskers on kittens
+// [bright copper kettles, warm woolen mittens, Brown paper packages tied up with strings]
+
+[10,30,20];
+
+const obj = {
+numbers: {
+    a: 1,
+    b: 2
+}
+};
+
+const {numbers: {a,b}} = obj;
+
+const arr = [1,2];
+ [arr[0],arr[1]] = [arr[1],arr[0]];
+
+
+ const raceResults= ([first,second,third,...rest]) => {return {first,second,third,rest}};
+
+ 
+
+ 
+
+
+
